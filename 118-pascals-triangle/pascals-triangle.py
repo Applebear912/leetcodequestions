9d0d916fe92114f804140ac513712197b94ffeab
Pingping Xiao <72316763+Applebear912@users.noreply.github.com>
2024-05-31 12:46:17 -0700

    Time: 20 ms (23.11%) | Memory: 11.6 MB (40.71%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -7,16 +7,15 @@ class Solution(object):
         row = 1
         result = []
         result.append([1])
-
+        pre_array = []
         while row < numRows:
             row += 1
-            current_array = [1] * row
-
+            current_array = []
             for i in range(row):
                 if i == 0 or i == row-1:
-                    current_array[i] = 1
+                    current_array.append(1)
                 else:
-                    current_array[i] = pre_array[i-1] + pre_array[i]
+                    current_array.append(pre_array[i-1] + pre_array[i])
             pre_array = current_array
             result.append(current_array)
 
