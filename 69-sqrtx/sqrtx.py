a39043d6b980e45489b5edf7b86bace7a6c356d8
Pingping Xiao <72316763+Applebear912@users.noreply.github.com>
2024-06-16 13:24:23 -0700

    Time: 21 ms (42.96%) | Memory: 11.5 MB (90.46%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -1,24 +1,39 @@
 class Solution(object):
+    def binarysearch(self, left, right, x):
+        mid = (left + right) // 2
+        if mid * mid <= x and (mid + 1) * (mid + 1) > x:
+            return mid
+        elif mid * mid < x:
+            return self.binarysearch(mid + 1, right, x)
+        else:
+            return self.binarysearch(left, mid - 1, x)
+
     def mySqrt(self, x):
-        """
-        :type x: int
-        :rtype: int
-        """
         if x == 0:
             return 0
         if x == 1:
             return 1
+        return self.binarysearch(0, x // 2, x)
+    # def mySqrt(self, x):
+    #     """
+    #     :type x: int
+    #     :rtype: int
+    #     """
+    #     if x == 0:
+    #         return 0
+    #     if x == 1:
+    #         return 1
         
-        left = 0
-        right = x // 2
-        while right >= left:
-            mid = (left + right) // 2
-            if mid * mid <= x and (mid + 1) * (mid + 1) > x:
-                return mid
-            elif mid * mid < x:
-                left = mid + 1
-            else:
-                right = mid - 1
+    #     left = 0
+    #     right = x // 2
+    #     while right >= left:
+    #         mid = (left + right) // 2
+    #         if mid * mid <= x and (mid + 1) * (mid + 1) > x:
+    #             return mid
+    #         elif mid * mid < x:
+    #             left = mid + 1
+    #         else:
+    #             right = mid - 1
 
 
         
