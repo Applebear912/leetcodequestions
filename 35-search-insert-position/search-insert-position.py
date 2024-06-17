6dbdff1f88a3578b615e87eb553b515540963fed
Pingping Xiao <72316763+Applebear912@users.noreply.github.com>
2024-06-16 18:18:17 -0700

    Time: 36 ms (10.26%) | Memory: 12.2 MB (82.06%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -1,33 +1,27 @@
 class Solution(object):
-    def binary(self, left, right, nums, target):
-        mid = (left + right) // 2
-        if nums[mid] == target:
-            return mid
-
-        if nums[mid] < target:
-
-            # Out of bounds
-            if mid + 1 >= len(nums):
-                return mid + 1
-
-            if target < nums[mid + 1]:
-                return mid + 1
-            return self.binary(mid + 1, right, nums, target)
-
-
-        # Target is smaller than nums[mid]
-        # Out of bounds
-        if mid - 1 < 0:
-            return mid
-
-        if target > nums[mid - 1]:
-            return mid
-
-        return self.binary(left, mid - 1, nums, target)
     def searchInsert(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: int
         """
-        return self.binary(0, len(nums)-1, nums, target)
+        left, right = 0, len(nums)-1
+
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+
+            if nums[mid] < target:
+                if mid + 1 >= len(nums):
+                    return mid + 1
+                if target < nums[mid + 1]:
+                    return mid + 1
+                left = mid + 1
+
+            if nums[mid] > target:
+                if mid - 1 < 0:
+                    return mid
+                if target > nums[mid - 1]:
+                    return mid
+                right = mid - 1
