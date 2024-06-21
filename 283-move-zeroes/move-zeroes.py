7c18bd073347f3379ac8c58416198fd10f85a92c
Pingping Xiao <72316763+Applebear912@users.noreply.github.com>
2024-06-21 12:31:29 -0700

    Time: 108 ms (90.31%) | Memory: 12.7 MB (83.82%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -5,9 +5,6 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums in-place instead.
         """
         pointer = 0
-        # loop through nums, check if current pointer is pointing to real num, if it is move to next
-        # when locate a zero,  look for next non-zero values, and swap it with current pointer
-        # break the loop when pointer == len of num
         if len(nums) == 1:
             return nums
 
