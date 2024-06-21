90ddb63ed7f4113bcde5b357f28fd12715525a09
Pingping Xiao <72316763+Applebear912@users.noreply.github.com>
2024-06-21 14:40:21 -0700

    Time: 777 ms (48.70%) | Memory: 22.9 MB (20.15%) - LeetSync

---
+++ 932-monotonic-array/monotonic-array.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isMonotonic(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        increasing = False
+        decreasing = False
+        for i in range(len(nums)-1):
+            if nums[i] < nums[i+1]:
+                increasing = True
+            if nums[i] > nums[i+1]:
+                decreasing = True
+        if increasing and decreasing:
+            return False
+        return True
