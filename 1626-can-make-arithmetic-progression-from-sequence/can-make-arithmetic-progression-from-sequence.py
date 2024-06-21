0525f9d4fa9747c4c9c8ac4e16a6624023958016
Pingping Xiao <72316763+Applebear912@users.noreply.github.com>
2024-06-21 14:15:48 -0700

    Time: 18 ms (80.86%) | Memory: 11.8 MB (11.33%) - LeetSync

---
+++ 1626-can-make-arithmetic-progression-from-sequence/can-make-arithmetic-progression-from-sequence.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def canMakeArithmeticProgression(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+    # sort array
+    # check difference are the same
+        arr.sort()
+        difference = arr[1] - arr[0]
+
+        for i in range(len(arr) - 1):
+            if arr[i] + difference != arr[i+1]:
+                return False
+        return True
