6147b628380be388d102a97a36b5329c71dcce4c
Pingping Xiao <72316763+Applebear912@users.noreply.github.com>
2024-06-21 12:28:10 -0700

    Time: 113 ms (74.18%) | Memory: 12.8 MB (83.82%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -4,20 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        # maintaining the relative order of non-zeros
-        # I: array
-        # O: in place, array, move the 0s to the end
+        pointer = 0
+        # loop through nums, check if current pointer is pointing to real num, if it is move to next
+        # when locate a zero,  look for next non-zero values, and swap it with current pointer
+        # break the loop when pointer == len of num
+        if len(nums) == 1:
+            return nums
 
-        if len(nums) == 0:
-            return []
-        
         for i in range(len(nums)):
-            if nums[i] == 0:
-                for j in range(i + 1, len(nums)):
-                    if nums[j] != 0:
-                        nums[i] = nums[j]
-                        nums[j] = 0
-                        break
-        return nums
+            if nums[i] != 0:
+                temp = nums[pointer]
+                nums[pointer] = nums[i]
+                nums[i] = temp
+                pointer += 1
 
-                    
+        return nums
