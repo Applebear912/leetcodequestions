8254401855bf25d4feda6e87bebd7f405022bb9b
Pingping Xiao <72316763+Applebear912@users.noreply.github.com>
2024-06-22 22:31:06 -0700

    Time: 18 ms (67.93%) | Memory: 11.8 MB (21.66%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -1,8 +1,3 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution(object):
     def mergeTwoLists(self, list1, list2):
         """
@@ -10,39 +5,16 @@ class Solution(object):
         :type list2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
+        head = ListNode()
+        cur = head
+        while list1 and list2:
+            if list1.val < list2.val:
+                cur.next = list1
+                list1 = list1.next
+            else:
+                cur.next = list2
+                list2 = list2.next
+            cur = cur.next
 
-        p1 = list1
-        p2 = list2
-
-        new_ll_head = None
-        tail = None
-        while p1 and p2:
-            if p1.val < p2.val:
-                if not new_ll_head:
-                    new_ll_head = p1
-                    tail = p1
-                else:
-                    tail.next = p1
-                    tail = tail.next
-                p1 = p1.next
-            else:
-                if not new_ll_head:
-                    new_ll_head = p2
-                    tail = p2
-                else:
-                    tail.next = p2
-                    tail = tail.next
-                p2 = p2.next
-        if p1:
-            if not new_ll_head:
-                new_ll_head = p1
-                tail = p1
-            else:
-                tail.next = p1
-        if p2:
-            if not new_ll_head:
-                new_ll_head = p2
-                tail = p2
-            else:
-                tail.next = p2
-        return new_ll_head
+        cur.next = list1 or list2
+        return head.next
