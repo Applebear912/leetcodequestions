f7b550d3ae42939a0bfcc29750778b896dd8feae
Pingping Xiao <72316763+Applebear912@users.noreply.github.com>
2024-06-22 22:27:07 -0700

    Time: 21 ms (44.41%) | Memory: 11.7 MB (21.66%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -10,25 +10,39 @@ class Solution(object):
         :type list2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        head = ListNode()
-        current = head
-        # initiate empty head to start
 
-        while list1 and list2:
-            # when one list reach to the end there is no need to continue the loop
-            if list1.val < list2.val:
-                # small one goes into the new list, having current head pointing to it
-                current.next = list1
-                # 
-                list1 = list1.next
+        p1 = list1
+        p2 = list2
+
+        new_ll_head = None
+        tail = None
+        while p1 and p2:
+            if p1.val < p2.val:
+                if not new_ll_head:
+                    new_ll_head = p1
+                    tail = p1
+                else:
+                    tail.next = p1
+                    tail = tail.next
+                p1 = p1.next
             else:
-                current.next = list2
-                list2 = list2.next
-            current = current.next
-        
-        current.next = list1 or list2
-        return head.next
-
-                
-                               
-        
+                if not new_ll_head:
+                    new_ll_head = p2
+                    tail = p2
+                else:
+                    tail.next = p2
+                    tail = tail.next
+                p2 = p2.next
+        if p1:
+            if not new_ll_head:
+                new_ll_head = p1
+                tail = p1
+            else:
+                tail.next = p1
+        if p2:
+            if not new_ll_head:
+                new_ll_head = p2
+                tail = p2
+            else:
+                tail.next = p2
+        return new_ll_head
