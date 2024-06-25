0c92c5dc14097202840de394c28b724cf0f8e113
Pingping Xiao <72316763+Applebear912@users.noreply.github.com>
2024-06-25 11:08:40 -0700

    Time: 23 ms, Memory: 16 MB - LeetHub

--- validate-binary-search-tree/validate-binary-search-tree.py
+++ validate-binary-search-tree/validate-binary-search-tree.py
@@ -11,14 +11,11 @@ class Solution(object):
         :rtype: bool
         """
         def valid(node, left, right):
-            if node == None:
+            if not node:
                 return True
-            if not (node.val < right and node.val > left):
+            if not (node.val > left and node.val < right):
                 return False
-            return (valid(node.left, left, node.val) and 
+
+            return (valid(node.left, left, node.val) and
                     valid(node.right, node.val, right))
         return valid(root, float('-inf'), float('inf'))
-
-    # The left subtree of a node contains only nodes with keys less than the node's key.
-    # The right subtree of a node contains only nodes with keys greater than the node's key.
-    # Both the left and right subtrees must also be binary search trees.
