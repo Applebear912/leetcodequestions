b227c562ceb8520b367c1b6f2858c075ebcde952
Pingping Xiao <72316763+Applebear912@users.noreply.github.com>
2025-03-31 13:31:53 -0700

    Time: 0 ms (100.00%) | Memory: 18.8 MB (51.38%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,14 +1,10 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        hash_map = {}
-
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        obj = {}
         for i in range(len(nums)):
-            if target - nums[i] in hash_map:
-                return [hash_map[target-nums[i]], i]
-            hash_map[nums[i]] = i
+            if target-nums[i] in obj:
+                return [obj[target-nums[i]], i]
+            obj[nums[i]] = i
+            
+
 
